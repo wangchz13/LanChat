98c6622cea5e0fbd7f29ffcbd23f2678f1b79c3e
yizhixiaobai <1042897963@qq.com>
2016-08-20 16:32:34 +0800

    存放各类通信相关

---
+++ Network/sendmessage.h
@@ -0,0 +1,33 @@
+﻿#ifndef SENDMESSAGE_H
+#define SENDMESSAGE_H
+
+#include "protocol.h"
+#include <QObject>
+
+class MessageSender : public QObject
+{
+    Q_OBJECT
+public:
+    explicit MessageSender(QObject *parent = 0);
+    MessageSender(M_Message message);
+    MessageSender(M_Login login);
+
+    QString getIP();
+    QString getUserName();
+    qint16 port=45454;
+signals:
+    void newBuddy();
+    void newMessage();
+    void buddyLeft();
+
+public slots:
+    void send(QHostAddress ip=QHostAddress::Broadcast);//默认发送给网关
+
+private:
+    QByteArray data;
+    MessageType _type;
+    M_Message _message;
+    M_Login _login;
+};
+
+#endif // SENDMESSAGE_H
