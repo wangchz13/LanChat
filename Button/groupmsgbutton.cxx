a208206135c337864373d708eac58d25c2bdcfdd
yizhixiaobai <1042897963@qq.com>
2016-08-22 17:13:34 +0800

    -

--- Button/groupmsgbutton.cxx
+++ Button/groupmsgbutton.cxx
@@ -3,8 +3,8 @@
 GroupMsgButton::GroupMsgButton(M_Message msg):
     MessageButton(msg._sender._name,msg._sender._data,msg._time/*,msg._sender._head*/)
 {
-    _profile->_name = msg._sender._name;
-    _profile->_data = msg._sender._data;
+//    _profile->_name = msg._sender._name;
+//    _profile->_data = msg._sender._data;
     /*头像未设置*/
     _timeLabel = new QLabel(_time);
     _countLabel = new QLabel;
