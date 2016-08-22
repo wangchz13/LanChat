e8052e7a72db947e990d6f9787eca2a06d367e03
yizhixiaobai <1042897963@qq.com>
2016-08-22 17:17:00 +0800

    --

--- Profile/contactprofile.cxx
+++ Profile/contactprofile.cxx
@@ -7,6 +7,8 @@ ContactProfile::ContactProfile()
 
 ContactProfile::ContactProfile(QString userName, QString computerName, QString ip, QImage head):
     BaseProfile(userName+"["+computerName+"]", ip, head),
+    _userName(userName),
+    _computerName(computerName),
     _ipAddress(ip)
 {
 
