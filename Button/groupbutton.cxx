7e279469150a2019dcc4bcbe4f7ebe1e8ba93c32
yizhixiaobai <1042897963@qq.com>
2016-08-22 15:23:20 +0800

    ---

--- Button/groupbutton.cxx
+++ Button/groupbutton.cxx
@@ -9,12 +9,12 @@ GroupButton::GroupButton(GroupProfile group):
     ProfileButton(group._name, group._data, group._head),
     _group(group)
 {
-    _type = ProfileType::contact;
+    _type = ProfileType::group;
 }
 
 void GroupButton::mouseDoubleClickEvent(QMouseEvent *e)
 {
-    emit newChat(BaseProfile(_group._name, _group._data, _group._head));
+    emit newChat(BaseProfile(_group._name, _group._data, _group._head), ProfileType::group);
 }
 
 void GroupButton::mouseReleaseEvent(QMouseEvent *e)
