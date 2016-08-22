8a009146f2612ff5db3ac6b7966bca49f608eada
yizhixiaobai <1042897963@qq.com>
2016-08-22 15:24:49 +0800

    --

---
+++ Network/filereceiver.h
@@ -0,0 +1,17 @@
+#ifndef FILERECEIVER_H
+#define FILERECEIVER_H
+
+#include <QObject>
+
+class FileReceiver : public QObject
+{
+    Q_OBJECT
+public:
+    explicit FileReceiver(QObject *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // FILERECEIVER_H
