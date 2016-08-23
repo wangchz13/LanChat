f9cf7ac79272495f95c883c99bb188d8a46b72b2
yizhixiaobai <1042897963@qq.com>
2016-08-23 10:12:28 +0800

    完善GroupButton

--- Button/groupbutton.cxx
+++ Button/groupbutton.cxx
@@ -1,6 +1,23 @@
-#include "groupbutton.h"
+﻿#include "groupbutton.h"
 
 GroupButton::GroupButton()
+{
+
+}
+
+GroupButton::GroupButton(GroupProfile group):
+    ProfileButton(group._name, group._data, group._head),
+    _group(group)
+{
+    _type = ProfileType::contact;
+}
+
+void GroupButton::mouseDoubleClickEvent(QMouseEvent *e)
+{
+
+}
+
+void GroupButton::mouseReleaseEvent(QMouseEvent *e)
 {
 
 }
