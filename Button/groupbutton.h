c81bc184e0a268b06e3dbb3619367095889036a1
yizhixiaobai <1042897963@qq.com>
2016-08-23 10:12:51 +0800

    完善

--- Button/groupbutton.h
+++ Button/groupbutton.h
@@ -1,11 +1,26 @@
-#ifndef GROUPBUTTON_H
+﻿#ifndef GROUPBUTTON_H
 #define GROUPBUTTON_H
 
+#include "Button/profilebutton.h"
+#include "Profile/groupprofile.h"
 
-class GroupButton
+#include "global.h"
+
+class GroupButton : public ProfileButton
 {
+    Q_OBJECT
 public:
     GroupButton();
+    GroupButton(GroupProfile group);
+
+    void mouseDoubleClickEvent(QMouseEvent *e);
+    void mouseReleaseEvent(QMouseEvent *e);
+
+signals:
+    void newChat(GroupProfile g);
+
+private:
+    GroupProfile _group;
 };
 
 #endif // GROUPBUTTON_H
