c3ccc054742fca2954a7fa284f6304e9069b01e3
yizhixiaobai <1042897963@qq.com>
2016-08-23 10:13:25 +0800

    添加注释

--- Button/messagebutton.h
+++ Button/messagebutton.h
@@ -1,10 +1,12 @@
-﻿/**************************
+﻿/*****************************************************************
 **
 ** 类名：MessageButton
 ** 父类：BaseButton
-** 目的：显示消息，包括群消息和个人消息
+** 目的：显示消息，包括群消息和个人消息，在BaseButton的基础上有所扩展，
+**      多了_timeLabel和_countLabel两个widget以及_time和_count两个
+**      数据成员。
 **
-***************************/
+*****************************************************************/
 #ifndef MESSAGEBOX_H
 #define MESSAGEBOX_H
 
@@ -15,13 +17,16 @@
 class MessageButton : public BaseButton
 {
 public:
-    //MessageButton(QString title, QString data, QString time, QImage head = defaultHead);
-    MessageButton(M_Message message);
-    MessageButton(QString title, QString data, QString time);
+    MessageButton(M_Message msg);
+    MessageButton(QString title, QString data, QString time, QImage head = defaultHead);
+    ~MessageButton();
+
     virtual void mouseDoubleClickEvent(QMouseEvent *e){}
     virtual void mouseReleaseEvent(QMouseEvent *e){}
+
 public slots:
     void refresh();
+
 public:
     QString _time;
     quint16 _count;
